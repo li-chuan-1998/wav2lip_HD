4acacda556773d6ba18053a493dc3eff7d415cb7
li-chuan-1998 <lchuan1998@gmail.com>
2023-06-29 01:59:05 +0800

    update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import gdown
 import os
 
-id = "1dM2ZurB8bKBYepsQdMjKxmqiyQYlmPJh"
-output = "./checkpoints/ckp_ls.pth"
-os.makedirs(output, exist_ok=True)
-gdown.download(id=id, output=output, quiet=False)
+id = "1Wdbg7jr10iJfXAkcZNS_mDM463SIrurg"
+output_dir = "./ckp_ls/"
+os.makedirs(output_dir, exist_ok=True)
+gdown.download(id=id, output=output_dir, quiet=False)
